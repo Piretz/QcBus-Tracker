d2ee7c01521c4b2fd580f0e5903bb918d3acf292
Piretz <94498274+Piretz@users.noreply.github.com>
2025-07-09 14:03:20 -1200

    update

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -39,7 +39,8 @@ const handleSubmit = async (e: React.FormEvent) => {
     // ✅ Validate inputs
     await validationSchema.validate(form, { abortEarly: false });
 
-    const res = await fetch('http://localhost:5000/api/auth/register', {
+  const res = await fetch('https://bus-tracker-be.onrender.com/api/auth/register', {
+
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form),
